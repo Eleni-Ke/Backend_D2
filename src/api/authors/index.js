8fe6915e5236bb4c7042437a0847daaf84774efa
Eleni-Ke <97251568+Eleni-Ke@users.noreply.github.com>
2023-03-05 19:28:45 +0100

    more changes

--- src/api/authors/index.js
+++ src/api/authors/index.js
@@ -1,43 +1,38 @@
 import Express from "express";
-import fs from "fs";
-import { fileURLToPath } from "url";
-import { dirname, join } from "path";
 import uniqid from "uniqid";
 import { getAuthors, writeAuthors } from "../../lib/fs-tools.js";
+import {
+  checkAuthorSchema,
+  triggerBadRequest,
+} from "../blogposts/blogpostSchema.js";
 
 const authorsRouter = Express.Router();
 
-const authorsJSONPath = join(
-  dirname(fileURLToPath(import.meta.url)),
-  "authors.json"
-);
+authorsRouter.post(
+  "/",
+  checkAuthorSchema,
+  triggerBadRequest,
+  async (req, res, next) => {
+    try {
+      const allAuthors = await getAuthors();
+      const newAuthor = {
+        ...req.body,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        id: uniqid(),
+      };
 
-// const writeAuthors = (allAuthorsArr) => {
-//   fs.writeFileSync(authorsJSONPath, JSON.stringify(allAuthorsArr));
-// };
+      allAuthors.push(newAuthor);
+      await writeAuthors(allAuthors);
 
-// const getAuthors = () => JSON.parse(fs.readFileSync(authorsJSONPath));
+      res.status(201).send({ id: newAuthor.id });
+    } catch (error) {
+      next(error);
+    }
 
-authorsRouter.post("/", async (req, res) => {
-  try {
-    const allAuthors = await getAuthors();
-    const newAuthor = {
-      ...req.body,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      id: uniqid(),
-    };
-
-    allAuthors.push(newAuthor);
-    await writeAuthors(allAuthors);
-
-    res.status(201).send({ id: newAuthor.id });
-  } catch (error) {
-    console.log(error);
+    //   res.send();
   }
-
-  //   res.send();
-});
+);
 
 authorsRouter.get("/", async (req, res) => {
   try {
@@ -60,20 +55,25 @@ authorsRouter.get("/:authorsId", async (req, res) => {
   }
 });
 
-authorsRouter.put("/:authorsId", async (req, res) => {
-  try {
-    const authorId = req.params.authorsId;
-    const allAuthors = await getAuthors();
-    const index = allAuthors.findIndex((author) => author.id === authorId);
-    const oldAuthor = allAuthors[index];
-    const updatedUser = { ...oldAuthor, ...req.body, updatedAt: new Date() };
-    allAuthors[index] = updatedUser;
-    await writeAuthors(allAuthors);
-    res.send(updatedUser);
-  } catch (error) {
-    console.log(error);
+authorsRouter.put(
+  "/:authorsId",
+  checkAuthorSchema,
+  triggerBadRequest,
+  async (req, res) => {
+    try {
+      const authorId = req.params.authorsId;
+      const allAuthors = await getAuthors();
+      const index = allAuthors.findIndex((author) => author.id === authorId);
+      const oldAuthor = allAuthors[index];
+      const updatedUser = { ...oldAuthor, ...req.body, updatedAt: new Date() };
+      allAuthors[index] = updatedUser;
+      await writeAuthors(allAuthors);
+      res.send(updatedUser);
+    } catch (error) {
+      console.log(error);
+    }
   }
-});
+);
 
 authorsRouter.delete("/:authorsId", async (req, res) => {
   const authorId = req.params.authorsId;
--- src/api/blogposts/blogpostSchema.js
+++ src/api/blogposts/blogpostSchema.js
@@ -56,7 +56,35 @@ const blogpostsSchema = {
   },
 };
 
+const authorsSchema = {
+  name: {
+    in: ["body"],
+    isString: {
+      errorMessage: "Name is a mandatory field and needs to be a string!",
+    },
+  },
+  surname: {
+    in: ["body"],
+    isString: {
+      errorMessage: "Surname is a mandatory field and needs to be a string!",
+    },
+  },
+  email: {
+    in: ["body"],
+    isString: {
+      errorMessage: "Email is a mandatory field and needs to be a string!",
+    },
+  },
+  DOB: {
+    in: ["body"],
+    isDate: {
+      errorMessage: "Date of birth value is required and must be a date",
+    },
+  },
+};
+
 export const checkBlogpostsSchema = checkSchema(blogpostsSchema);
+export const checkAuthorSchema = checkSchema(authorsSchema);
 
 export const triggerBadRequest = (req, res, next) => {
   const errors = validationResult(req);
@@ -67,7 +95,7 @@ export const triggerBadRequest = (req, res, next) => {
     next();
   } else {
     next(
-      createHttpError(400, "Errors during post validation", {
+      createHttpError(400, "Errors during post/put validation", {
         errorsList: errors.array(),
       })
     );
