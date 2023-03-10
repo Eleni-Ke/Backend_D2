19fceb887f4ca27db427fb10dec65ed1f2f1a72b
Eleni-Ke <97251568+Eleni-Ke@users.noreply.github.com>
2023-03-10 09:41:46 +0100

    console.log added

--- src/lib/email-tools.js
+++ src/lib/email-tools.js
@@ -2,6 +2,7 @@ import sgMail from "@sendgrid/mail";
 sgMail.setApiKey(process.env.SENDGRID_KEY);
 
 export const sendsPostEmail = async (recipientAdress) => {
+  console.log(recipientAdress);
   const msg = {
     to: recipientAdress,
     from: process.env.SENDER_EMAIL_ADDRESS,
